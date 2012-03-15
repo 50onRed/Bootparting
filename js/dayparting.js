5a3afef0fcab2246f053d518da07dd831a606986
Graham <graham@50onred.com>
2012-03-15 11:52:06 -0400

    add row/col toggling

--- js/dayparting.js
+++ js/dayparting.js
@@ -8,18 +8,50 @@
 		element: null,
 		days: {0: 'Sun',1: 'Mon',2: 'Tue',3: 'Wed',4: 'Thu',5: 'Fri',6: 'Sat'},
 		hours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+		headers: null,
 		rows: null,
 		cols: null,
 
 		init: function(type, element, options) {
 			this.element = element;
+			this.headers = {'hours': [], 'days': []};
 			this.rows = {};
 			this.cols = {};
 			
 			this.element.appendChild(this.create_head());
 			this.element.appendChild(this.create_body());
+			this.wire_headers();
 		},
 
+		/* Wire up the header checkboxes so that they function as toggles for rows & cols */
+		wire_headers: function() {
+			var daypart = this;
+			for (hour_index in this.headers['hours']) {
+				var hour_key = hour_index;
+				this.headers['hours'][hour_index].onchange = (function(hour_key, daypart) {
+					return function() {
+						var newval = daypart.headers['hours'][hour_key].checked;
+						for (day_key in daypart.rows[hour_key]) {
+							daypart.rows[hour_key][day_key].checked = newval;
+						}
+					};
+				})(hour_key, daypart);
+			}
+
+			for (day_index in this.headers['days']) {
+				var day_key = day_index;
+				this.headers['days'][day_index].onchange = (function(day_key, daypart) {
+					return function() {
+						var newval = daypart.headers['days'][day_key].checked;
+						for (hour_key in daypart.cols[day_key]) {
+							daypart.cols[day_key][hour_key].checked = newval;
+						}
+					}
+				})(day_key, daypart);
+			}
+		},
+
+		/* Create the thead element and daily column headers */
 		create_head: function() {
 			var thead = document.createElement('thead');
 			var tr = document.createElement('tr');
@@ -28,7 +60,22 @@
 
 			for (var day_index in this.days) {
 				var th = document.createElement('th');
-				th.innerText = this.days[day_index];
+				var label = document.createElement('label');
+				var input_id = 'day_' + day_index + '_header';
+				$(label).attr('for', input_id);
+				label.innerText = ' ' + this.days[day_index];
+				
+				var checkbox = document.createElement('input');
+				checkbox.type = 'checkbox';
+				checkbox.id = input_id;
+				checkbox.checked = true;
+				checkbox.className = 'pull-left';
+
+				this.headers['days'][day_index] = checkbox;
+
+				th.appendChild(checkbox);
+				th.appendChild(label);
+
 				tr.appendChild(th);
 			}
 
@@ -45,13 +92,30 @@
 				var hour = this.hours[hour_index];
 	
 				var td = document.createElement('td');
-				td.innerText = this.get_display_hour(hour);
+
+				var input_id = 'hour_' + hour_index + '_check';
+				var label = document.createElement('label');
+				$(label).attr('for', input_id);
+				label.innerText = this.get_display_hour(hour) + ' ';
+				label.className = 'pull-left';
+
+				var check = document.createElement('input');
+				check.type = 'checkbox';
+				check.id = input_id;
+				check.checked = true;
+				this.headers['hours'][hour_index] = check;
+
+				td.appendChild(label);
+				td.appendChild(check);
+
 				tr.appendChild(td);
 					
 				for (var day_index in this.days) {
 					var td = document.createElement('td');
 					var check = document.createElement('input');
 					check.type = 'checkbox';
+					check.checked = true;
+					
 
 					this.rows[hour_index][day_index] = check;
 					if (!(day_index in this.cols)) {
